072e033fd400cb6f03f91f30f1e75833922b99d2
Anderson <andersonfonseca1999@hotmail.com>
2021-10-05 19:04:16 -0300

    Add global style

---
+++ src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from "styled-components";
+
+import MetropolisRegular from "assets/fonts/Metropolis-Regular.otf";
+import MetropolisMedium from "assets/fonts/Metropolis-Medium.otf";
+import MetropolisBlack from "assets/fonts/Metropolis-Black.otf";
+import MetropolisBold from "assets/fonts/Metropolis-Bold.otf";
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Metropolis Regular';
+    font-display: swap;
+    src: url(${MetropolisRegular});
+  }
+
+  @font-face {
+    font-family: 'Metropolis Medium';
+    font-display: swap;
+    src: url(${MetropolisMedium});
+  }
+
+  @font-face {
+    font-family: 'Metropolis Black';
+    font-display: swap;
+    src: url(${MetropolisBlack});
+  }
+
+  @font-face {
+    font-family: 'Metropolis Bold';
+    font-display: swap;
+    src: url(${MetropolisBold});
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Metropolis Regular', sans-serif;
+  }
+`;
+
+export default GlobalStyles;
