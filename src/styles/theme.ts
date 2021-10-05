a108d79d500bc21be218eff2b3076d70bbfd43d8
Anderson <andersonfonseca1999@hotmail.com>
2021-10-05 19:02:24 -0300

    Add style theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,39 @@
+export default {
+  font: {
+    sizes: {
+      xsmall: "1.2rem",
+      small: "1.4rem",
+      medium: "1.6rem",
+      large: "1.8rem",
+      xlarge: "2.0rem",
+      xxlarge: "2.8rem",
+    },
+  },
+  colors: {
+    primary: "#FE4400",
+    secondary: "#404099",
+    textColor: "#303033",
+    white: "#FFFFFF",
+    black: "#000000",
+  },
+  spacings: {
+    xxsmall: "0.8rem",
+    xsmall: "1.6rem",
+    small: "2.4rem",
+    medium: "3.2rem",
+    large: "4.0rem",
+    xlarge: "4.8rem",
+    xxlarge: "5.6rem",
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50,
+  },
+  transition: {
+    default: "0.3s ease-in-out",
+    fast: "0.1s ease-in-out",
+  },
+} as const;
