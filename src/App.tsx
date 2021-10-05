d50473b7daf0faeea6e6dd6845d7a7d6f4595af5
Anderson <andersonfonseca1999@hotmail.com>
2021-10-05 19:05:56 -0300

    Add ThemeProvider and GlobalStyles to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import GlobalStyles from "./styles/global";
+import { ThemeProvider } from "styled-components";
+import theme from "styles/theme";
+
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
