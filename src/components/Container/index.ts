282c5e481488b7648585683284deb847dbff91ba
Anderson <andersonfonseca1999@hotmail.com>
2021-10-05 19:17:15 -0300

    Add Container component

---
+++ src/components/Container/index.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/macro";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 120rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+`;
+
+export default Container;
