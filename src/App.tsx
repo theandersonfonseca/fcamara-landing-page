853ccca617ad533da73c4a95a6e710047c2fac6e
Anderson <andersonfonseca1999@hotmail.com>
2021-10-07 20:22:37 -0300

    Add components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,26 @@ import GlobalStyles from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import theme from "styles/theme";
 
+import Header from "components/Header";
+import IntroSection from "components/IntroSection";
+import Warning from "components/Warning";
+import InstructionSection from "components/InstructionSection";
+import PrecautionsSection from "components/PrecautionsSection";
+import Footer from "components/Footer";
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Header />
+
+      <main>
+        <IntroSection />
+        <Warning />
+        <InstructionSection />
+        <PrecautionsSection />
+      </main>
+
+      <Footer />
       <GlobalStyles />
     </ThemeProvider>
   );
