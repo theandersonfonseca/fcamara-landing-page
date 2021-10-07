78bd927fecfcc7969a3ca7d9d083dbd61e99c7a3
Anderson <andersonfonseca1999@hotmail.com>
2021-10-07 20:15:00 -0300

    Add mobile menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { ReactComponent as Logo } from "assets/images/logo.svg";
 import Container from "components/Container";
+import SocialMediaIcons from "components/SocialMediaIcons";
 
 import * as S from "./styles";
 
 export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <S.Wrapper>
       <Container>
@@ -17,7 +21,11 @@ export default function Header() {
         </a>
 
         <nav>
-          <S.Menu>
+          <S.Menu openMenu={openMenu}>
+            {openMenu && (
+              <S.CloseMenu onClick={() => setOpenMenu(false)}>X</S.CloseMenu>
+            )}
+
             <S.MenuItem>
               <a href="#sobre">Sobre</a>
             </S.MenuItem>
@@ -29,7 +37,17 @@ export default function Header() {
             <S.MenuItem>
               <a href="#precauções">Precauções</a>
             </S.MenuItem>
+
+            {openMenu && (
+              <SocialMediaIcons align="horizontal" color="primary" />
+            )}
           </S.Menu>
+
+          <S.MobileMenuHamburger onClick={() => setOpenMenu(true)}>
+            <S.MobileMenuLine />
+            <S.MobileMenuLine />
+            <S.MobileMenuLine />
+          </S.MobileMenuHamburger>
         </nav>
       </Container>
     </S.Wrapper>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components/macro";
 import Container from "components/Container";
+import { Wrapper as SocialMediaIcons } from "components/SocialMediaIcons/styles";
 
 export const Wrapper = styled.header`
   ${({ theme }) => css`
@@ -15,24 +16,40 @@ export const Wrapper = styled.header`
       justify-content: space-between;
       align-items: center;
       height: 100%;
-      padding: ${theme.spacings.xsmall} 0;
-
-      @media (max-width: 650px) {
-        flex-direction: column;
-      }
+      padding: ${theme.spacings.xsmall} 1.5rem;
     }
   `}
 `;
 
-export const Menu = styled.ul`
-  ${({ theme }) => css`
-    display: flex;
+type MenuProps = {
+  openMenu: boolean;
+};
+
+export const Menu = styled.ul<MenuProps>`
+  ${({ theme, openMenu }) => css`
     list-style: none;
+    display: flex;
+
+    ${SocialMediaIcons} {
+      display: none;
+    }
 
     @media (max-width: 650px) {
       flex-direction: column;
-      align-items: center;
-      margin-top: ${theme.spacings.small};
+      align-items: flex-end;
+      width: 30rem;
+      padding: ${theme.spacings.medium};
+      position: absolute;
+      top: 0;
+      right: ${openMenu ? "0" : "-100%"};
+      background: ${theme.colors.white};
+      transition: 0.4s;
+      box-shadow: 0 0 0.3rem #ccc;
+
+      ${SocialMediaIcons} {
+        display: flex;
+        margin-top: ${theme.spacings.medium};
+      }
     }
   `}
 `;
@@ -61,6 +78,59 @@ export const MenuItem = styled.li`
       @media (max-width: 650px) {
         padding: ${theme.spacings.xxsmall};
       }
+    }
+  `}
+`;
+
+export const MobileMenuIcon = styled.div`
+  ${({ theme }) => css`
+    background: red;
+
+    & svg {
+      stroke: ${theme.colors.textColor};
+      fill: ${theme.colors.textColor};
+    }
+  `}
+`;
+
+export const MobileMenuHamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 2.2rem;
+  height: 1.8rem;
+  cursor: pointer;
+
+  @media (max-width: 650px) {
+    display: flex;
+  }
+`;
+
+export const MobileMenuLine = styled.span`
+  ${({ theme }) => css`
+    height: 0.2rem;
+    width: 100%;
+    background: ${theme.colors.textColor};
+  `}
+`;
+
+export const CloseMenu = styled.span`
+  ${({ theme }) => css`
+    display: none;
+    justify-content: center;
+    align-items: center;
+    width: 4rem;
+    height: 4rem;
+    margin-bottom: ${theme.spacings.medium};
+    cursor: pointer;
+    font-family: "Metropolis Bold";
+    font-size: ${theme.font.sizes.large};
+    color: ${theme.colors.white};
+    background: ${theme.colors.secondary};
+    border-radius: 100%;
+
+    @media (max-width: 650px) {
+      display: flex;
     }
   `}
 `;
